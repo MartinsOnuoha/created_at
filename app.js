6912404f838afc040da4250840b4bdf2b8e36d5e
MartinsOnuoha <martinsvictor.nd@gmail.com>
2021-02-17 10:21:28 +0100

    adds readme file

--- app.js
+++ app.js
@@ -11,29 +11,6 @@ app.set('view engine', 'hbs')
 
 app.use('/', mainRoute);
 
-// app.get('/get-data', async (req, res) => {
-//   const url = req.query.url;
-//   if (url) {
-//     const result = await axios.get(`${url}`).then(result => result.data).catch(e => res.status(400).json({ msg: e, error: true }));
-//     const { full_name, private, description, html_url, created_at } = result
-
-//     return res.json({
-//       full_name,
-//       private,
-//       description,
-//       html_url,
-//       created_at: format(new Date(created_at), 'PPPpp'),
-//       how_old: new Date().getFullYear() - new Date(created_at).getFullYear(),
-//       avatar: result.owner.avatar_url,
-//     })
-//   }
-//   return res.status(400).json({ error: true, msg: 'provide a URL' })
-// })
-
-// app.get('/', (req, res) => {
-//   return res.render('index')
-// })
-
 app.listen(port, () => {
-  console.log(`Created at http://localhost:${port}`)
+  console.log(`Created at http://localhost:${port} 🚀`)
 })
